965af5f3feaa13d953f2119c68508bc71ce67b8d
Energeenot <abramov.pandos@yandex.ru>
2024-10-28 17:31:12 +0300

    Написал проверку на null для полей student

--- src/main/java/Energeenot/TestTaskSDS/services/StudentService.java
+++ src/main/java/Energeenot/TestTaskSDS/services/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -27,11 +28,12 @@ public class StudentService {
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
 
-        student.setSurname(updatedStudent.getSurname());
-        student.setName(updatedStudent.getName());
-        student.setPatronymic(updatedStudent.getPatronymic());
-        student.setGroup(updatedStudent.getGroup());
-        student.setAverageMark(updatedStudent.getAverageMark());
+        System.out.println("Updated Student: " + updatedStudent);
+        Optional.ofNullable(updatedStudent.getSurname()).ifPresent(student::setSurname);
+        Optional.ofNullable(updatedStudent.getName()).ifPresent(student::setName);
+        Optional.ofNullable(updatedStudent.getPatronymic()).ifPresent(student::setPatronymic);
+        Optional.ofNullable(updatedStudent.getGroup()).ifPresent(student::setGroup);
+        Optional.ofNullable(updatedStudent.getAverageMark()).ifPresent(student::setAverageMark);
         return studentRepository.save(student);
     }
 
